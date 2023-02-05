811ac845ac8c356d39a47fc64ef6c490186f86f1
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-02-05 10:58:37 -0800

    :sparkles: Adds storing temperature in cookies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
-import { TimeComponent } from './time/time.component';
-import { WeatherComponent } from './weather/weather.component';
-import { LoadingComponent } from './loading/loading.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SearchComponent} from './search/search.component';
+import {TimeComponent} from './time/time.component';
+import {WeatherComponent} from './weather/weather.component';
+import {LoadingComponent} from './loading/loading.component';
+import {CookieService} from "ngx-cookie-service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchComponent,
-    TimeComponent,
-    WeatherComponent,
-    LoadingComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SearchComponent,
+        TimeComponent,
+        WeatherComponent,
+        LoadingComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [CookieService],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,6 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import axios from "axios";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
     selector: 'app-weather',
@@ -7,34 +8,45 @@ import axios from "axios";
     styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent implements AfterViewInit {
-    @ViewChild("temp", {static: false}) tempElement?: ElementRef
-    loading: boolean = true
+    temp: string = ""
+
+    constructor(private cookieService: CookieService) {
+    }
 
     ngAfterViewInit() {
         this.getWeather()
     }
 
     public getWeather() {
+        if (this.cookieService.check("temp")) {
+            this.temp = `${this.cookieService.get("temp")} °C`
+            return
+        }
+
         navigator.geolocation.getCurrentPosition(async (data) => {
+            // success
             await this.loadWeather(data.coords.latitude, data.coords.longitude)
         }, async () => {
+            // error
             const location = await axios.get("https://www.geolocation-db.com/json/")
-            if (location.status === 200 && location.data?.latitude !== undefined && location.data?.longitude !== undefined)
-                await this.loadWeather(location.data.latitude, location.data.longitude)
-            else
-                this.loading = false
+            if (location.status !== 200 || location.data?.latitude === undefined || location.data?.longitude === undefined)
+                return
+
+            await this.loadWeather(location.data.latitude, location.data.longitude)
         })
     }
 
     public async loadWeather(latitude?: number, longitude?: number) {
-        if (latitude === undefined || longitude === undefined) {
-            this.loading = false
+        if (latitude === undefined || longitude === undefined)
             return
-        }
 
         const weather = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&temperature_unit=celsius`)
-        this.loading = false;
-        if (weather.status === 200 && weather.data?.current_weather?.temperature !== undefined)
-            this.tempElement!!.nativeElement.innerText = `${Math.floor(weather.data.current_weather.temperature)} °C`
+        if (weather.status !== 200 || weather.data?.current_weather?.temperature === undefined)
+            return
+
+        this.temp = `${Math.floor(weather.data.current_weather.temperature)} °C`
+        const expires = new Date()
+        expires.setHours(expires.getHours() + 1)
+        this.cookieService.set("temp", JSON.stringify(weather.data.current_weather.temperature), expires)
     }
 }
