0ba1472cccd615ed171f43c082a61ef87a2a4571
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-02-04 18:55:59 -0800

    :sparkles: Adds time and date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core'
+import * as moment from 'moment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+    @ViewChild("time") timeElement?: ElementRef;
+    @ViewChild("date") dateElement?: ElementRef;
+
+    ngAfterViewInit() {
+        this.updateTime()
+        setInterval(this.updateTime, 500);
+    }
+
+    private updateTime() {
+        this.timeElement!!.nativeElement.innerText = moment().format("[It's] h:mm A")
+        this.dateElement!!.nativeElement.innerText = moment().format("ddd MMM D, YYYY")
+    }
+
+    public search(query: string) {
+        window.location.href = `https://www.google.com/search?q=${query}`
+    }
+
+    public openGoogle() {
+        window.location.href = "https://www.google.com/"
+    }
+}
