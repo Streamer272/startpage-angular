94490079e65220cc58c16dc2310231041de3d720
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2023-02-07 07:54:06 -0800

    :zap: Implements time settings

--- src/app/time/time.component.ts
+++ src/app/time/time.component.ts
@@ -8,10 +8,43 @@ import {SettingsService} from "../settings/settings.service";
     styleUrls: ['./time.component.scss']
 })
 export class TimeComponent {
+    greeting: string = ""
     time: string = ""
     date: string = ""
+    timeFormat: string = ""
+    dateFormat: string = ""
 
     constructor(public settingsService: SettingsService) {
+        // intro
+        const addIts = this.settingsService.getSetting("enableTime") || this.settingsService.getSetting("enableDate")
+        if (this.settingsService.getSetting("enableGreeting")) {
+            const date = new Date()
+            if (0 <= date.getHours() && date.getHours() < 6)
+                this.greeting = "Good night"
+            else if (6 <= date.getHours() && date.getHours() < 12)
+                this.greeting = "Good morning"
+            else if (12 <= date.getHours() && date.getHours() < 18)
+                this.greeting = "Good afternoon"
+            else if (18 <= date.getHours() && date.getHours() < 24)
+                this.greeting = "Good evening"
+
+            if (addIts)
+                this.greeting += ", it's"
+        } else {
+            if (addIts)
+                this.greeting = "It's"
+        }
+
+        // time
+        this.timeFormat = this.settingsService.getSetting("hourFormat") === this.settingsService.OPTIONS["hourFormat"][0] ?
+            `h:mm${this.settingsService.getSetting("showSeconds") ? ':ss' : ''} A` : `HH:mm${this.settingsService.getSetting("showSeconds") ? ':ss' : ''}`
+
+        // date
+        // TODO: fix
+        const day = ["ddd ", "dddd ", " "].at(this.settingsService.OPTIONS["dayFormat"].indexOf(this.settingsService.getSetting("dayFormat")))
+        const month = ["MMM", "MMMM"].at(this.settingsService.OPTIONS["dayFormat"].indexOf(this.settingsService.getSetting("dayFormat")))
+        const year = [", YY", ", YYYY", ""].at(this.settingsService.OPTIONS["dayFormat"].indexOf(this.settingsService.getSetting("dayFormat")))
+        this.dateFormat = `${day}${month} D${year}`
     }
 
     ngOnInit() {
@@ -22,7 +55,7 @@ export class TimeComponent {
     }
 
     private updateTime() {
-        this.time = moment().format("h:mm A")
+        this.time = moment().format(this.timeFormat)
         this.date = moment().format("ddd MMM D, YYYY")
     }
 }
